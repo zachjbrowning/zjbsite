ae47d7c713657669f068061853789f9db2ce4e08
zachjbrowning <zachjbrowning@outlook.com>
2020-05-03 14:24:07 +1000

    all g now

--- zjbsite/frontend/src/components/Main/MainResume.js
+++ zjbsite/frontend/src/components/Main/MainResume.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function MainResume() {
     return (
         <div className="constrict resume-box welcome">
-                as well as my&nbsp;<a href="/static/ZJB-CV.pdf" target="_blank" className="inverse-flave linkage">Resume</a>...
+                <span>as well as my&nbsp;<a href="/static/ZJB-CV.pdf" target="_blank" className="inverse-flave linkage">Resume</a>...</span>
             <div id="resume-untitle">
                 (hire me pls)
             </div>
